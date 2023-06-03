2baf52a601e5733989b4b7bfe5753d51dd3677ae
Akarsh-J <76930147+Akarsh-J@users.noreply.github.com>
2023-06-03 11:58:56 +0530

    implemented initial multithreading logic

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from kafka import KafkaConsumer
+import threading
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+
 
 # Create a Kafka consumer instance
 consumer = KafkaConsumer(
@@ -6,23 +10,75 @@ consumer = KafkaConsumer(
     bootstrap_servers="localhost:9092",
     group_id="group-id",
     enable_auto_commit=False,  # Disable auto commit
-    max_poll_records=20,  # Set the maximum number of records to fetch in each poll
+    max_poll_records=18,  # Set the maximum number of records to fetch in each poll
 )
 
-# Continuously poll for messages in batches
-while True:
-    # Poll for new messages
-    batch = consumer.poll(timeout_ms=500)  # Adjust the timeout as needed
+num_threads = 4
+barrier = threading.Barrier(num_threads + 1)  # +1 for main thread
 
-    # distribute the messages from the batch to 4 threads and execute them concurrently
-    # Perform error handling and commiting of offset accordingly
-    # -----------------------
-    # -----------------------
-    # -----------------------
+# thread pool to limit the number of threads being sprout
+pool = ThreadPoolExecutor(max_workers=num_threads)
+
+# Store execution status of each offset
+offsets = {}
+
+
+def process_thread_records(records):
+    # wait for all threads to reach the barrier
+    barrier.wait()
+    for record in records:
+        try:
+            print(threading.current_thread().name)
+            print(record.offset)
+            offsets[record.offset] = 1
+        except Exception as e:
+            print("An error occured: ", str(e))
+            offset[record.offset] = -1
+
+
+# Continuously poll for thread_records in batches
+while True:
+    # Poll for new thread_records
+    batch = consumer.poll(timeout_ms=500)  # Adjust the timeout as needed
+    print("New Batch is here\n")
+    print("\n\n")
+
+    # threadpool
+    futures = []
     for topic_partition, records in batch.items():
+        offsets.clear
+
         for record in records:
-            print(record.value.decode("utf-8"))
-        print("Next batch\n\n")
+            offsets[record.offset] = 0
+
+        num_records = len(records)
+        min_records_per_thread = num_records // num_threads
+
+        for i in range(num_threads):
+            thread_records = []
+            index = i
+
+            # to populate thread_records with the appropriate records
+            # if records available are 0-19, thread 1 gets 0,4,8,12,16.
+            for j in range(min_records_per_thread + 1):
+                if index < num_records:
+                    thread_records.append(records[index])
+                    index += min_records_per_thread
+
+            # Submit the thread task to the thread pool
+            future = pool.submit(process_thread_records, thread_records)
+            futures.append(future)
+
+    print(offsets)
+    barrier.wait()
+
+    # wait for all threads to complete its execution
+    for future in futures:
+        future.result()
+    print("\n after join")
+    print(offsets)
+
+    a = input()
     # print(batch)
 
     # Manually commit the offsets once the batch is processed
