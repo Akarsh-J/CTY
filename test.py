393389eaf576f0bd05bf2fd487256c24fb933ff9
Akarsh-J <76930147+Akarsh-J@users.noreply.github.com>
2023-06-06 00:11:26 +0530

    Filled with issues

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import threading
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 from kafka.structs import TopicPartition
+import psycopg2
+import json
 
 
 # Create a Kafka consumer instance
@@ -32,18 +34,167 @@ executed_records_post_error = []
 
 
 def process_thread_records(records):
+    
+    connection = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="cty",
+        user="akarshj21",
+        password="AkArshj21",
+    )
+
+    cursor = connection.cursor()
+    
     # wait for all threads to reach the barrier
     barrier.wait()
     for record in records:
         #Check if the record was executed by the prvious batch
         if record.offset not in executed_records_post_error:
             try:
+                msg = record.value.decode("utf-8")
+                # msg = msg.strip().split(",")
+                msg = json.loads(msg)
+
+                # print(type(msg), msg)
+
+                db = msg["db"]
+                operation = msg["operation"]
+
+                if msg["db"] == "Client":
+                    (
+                        user_id,
+                        username,
+                        password,
+                        first_name,
+                        last_name,
+                        phone,
+                        address_1,
+                        address_2,
+                    ) = msg["data"].values()
+
+                    query = f"INSERT INTO Client VALUES ('{user_id}', '{username}', '{password}', '{first_name}', '{last_name}', '{phone}', '{address_1}', '{address_2}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Cart":
+                    cart_id, user_id, total_qty, total_cost, created_at, modified_at = msg[
+                        "data"
+                    ].values()
+
+                    query = f"INSERT INTO Cart VALUES ({cart_id}, '{user_id}', {total_qty}, {total_cost}, '{created_at}', '{modified_at}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Order_Details":
+                    order_id = msg["data"]["order_id"]
+                    user_id = msg["data"]["user_id"]
+                    total_cost = msg["data"]["total_cost"]
+                    items = json.dumps(msg["data"]["items"])
+                    payment_id = msg["data"]["Payment_id"]
+                    cart_id = msg["data"]["cart_id"]
+                    created_at = msg["data"]["Created_at"]
+                    modified_at = msg["data"]["Modified_at"]
+
+                    query = f"INSERT INTO Order_Details VALUES ({order_id}, '{user_id}', {total_cost}, '{items}', '{payment_id}', {cart_id}, '{created_at}', '{modified_at}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Payment_Details":
+                    (
+                        order_id,
+                        transaction_id,
+                        amount,
+                        provider,
+                        status,
+                        created_at,
+                        payment_id,
+                    ) = msg["data"].values()
+
+                    query = f"INSERT INTO Payment_Details VALUES ({order_id}, {transaction_id}, {amount}, '{provider}', '{status}', '{created_at}', '{payment_id}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "user_payment":
+                    payment_id, user_id, payment_type, provider, account_no, expiry = msg[
+                        "data"
+                    ].values()
+
+                    query = f"INSERT INTO user_payment VALUES ('{payment_id}', '{user_id}', '{payment_type}', '{provider}', '{account_no}', '{expiry}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Product":
+                    product_id = msg["data"]["Product_id"]
+                    product_name = msg["data"]["Product_name"]
+                    description = msg["data"]["description"]
+                    category = json.dumps(msg["data"]["Category"])
+                    price = msg["data"]["Price"]
+                    created_at = msg["data"]["Created_at"]
+                    modified_at = msg["data"]["modified_at"]
+
+                    query = f"INSERT INTO Product VALUES ('{product_id}', '{product_name}', '{description}', '{category}', {price}, '{created_at}', '{modified_at}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Vendor":
+                    vendor_id, vendor_name, password, phone, addr, created_at = msg[
+                        "data"
+                    ].values()
+
+                    query = f"INSERT INTO Vendor VALUES ('{vendor_id}', '{vendor_name}', '{password}', '{phone}', '{addr}', '{created_at}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "product_Vendor":
+                    (
+                        product_id,
+                        vendor_id,
+                        item_id,
+                        item_name,
+                        quantity,
+                        discount_price,
+                        created_at,
+                        modified_at,
+                    ) = msg["data"].values()
+
+                    query = f"INSERT INTO product_Vendor VALUES ('{product_id}', '{vendor_id}', '{item_id}', '{item_name}', {quantity}, {discount_price}, '{created_at}', '{modified_at}')"
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
+
+                if msg["db"] == "Cart_items":
+                    cart_id, item_id, quantity = msg["data"].values()
+
+                    query = (
+                        f"INSERT INTO Cart_items VALUES ({cart_id}, '{item_id}', {quantity})"
+                    )
+                    cursor.execute(query)
+                    connection.commit()
+
+                    print(f"Message '{query}' inserted into the database")
                 
                 print(threading.current_thread().name, record.offset)
                 # print(record.offset)
                 offsets[record.offset] = 1
+                
             except Exception as e:
-                print("An error occured: ", str(e))
+                #print("An error occured: ", str(e))
+                print(f"Error processing message '{record.value.decode('utf-8')}': {str(e)}")
                 offsets[record.offset] = -1
         
         else:
@@ -59,12 +210,15 @@ def commit_offsets():
     for key in sorted_keys:
         value = offsets[key]
         if value == 1 and error_occurred == False:
+            print(1)
             offset_to_commit = key
         #After an exception has occured if any record has been executed, Then keep track of those records in the list
-        elif value == 1 and error_occured == True:
+        elif value == 1 and error_occurred == True:
+            print(2)
             executed_records_post_error.append(key)
         elif value == -1:
-            error_occured = True
+            print(3)
+            error_occurred = True
 
     if offset_to_commit != -1:
         # move the consumer pointer to the offset that is to be committed
